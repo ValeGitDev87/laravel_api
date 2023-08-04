95ae76fa8f22b0c340b0ed2fd1cb3171703d46b4
Valentino Sciarnè <valentinosciarne@MBPdiValentino.homenet.telecomitalia.it>
2023-08-04 09:32:17 +0200

    factory data create

--- database/migrations/2023_08_04_055818_create_threads_table.php
+++ database/migrations/2023_08_04_055818_create_threads_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('threads', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string("title");
+            $table->text("body");
         });
     }
 
---
+++ note.md
@@ -0,0 +1,4 @@
+le factoy vengo usati per  testing al db ;
+utilizzo : 
+1) si lancia php artisan tinker cli che permette di dialogare con il db;
+2) comando per creare =  Thread::factory()->count(n)->create();
